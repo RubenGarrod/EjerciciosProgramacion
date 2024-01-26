9beff9eab04fbf3abd1567ffa466d6f8fdada23b
Ruben Garcia <ruben.garrod@educa.jcyl.es>
2024-01-26 21:00:12 +0100

    todo

--- EjerciciosTEMA8/src/cuerposcelestes/Ejercicio2Tema9.java
+++ EjerciciosTEMA8/src/cuerposcelestes/Ejercicio2Tema9.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ejerciciostema8;
+package cuerposcelestes;
 
 import cuerposcelestes.Planeta;
 import cuerposcelestes.Satelite;
