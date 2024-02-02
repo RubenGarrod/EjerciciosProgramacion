db907dbabe0a67424844e74616ca94ffd40972ed
RubenGarrod <151464813+RubenGarrod@users.noreply.github.com>
2024-02-02 16:16:07 +0100

    DawBank

---
+++ EjerciciosTEMA8/src/dawbank/CuentaBancaria.java
@@ -0,0 +1,108 @@
+package dawbank;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class CuentaBancaria {
+
+    private final String iban;
+    private final String titular;
+    private double saldo;
+    private final ArrayList<String> movimientos;
+    /**
+     * Constructor de la clase CuentaBancaria
+     * @param iban
+     * @param titular 
+     */
+    public CuentaBancaria(String iban, String titular) {
+        this.iban = iban;
+        this.titular = titular;
+        this.saldo = 0;
+        this.movimientos = new ArrayList<>();
+    }
+    /**
+     * Funcion que recibe una cantidad a ingresar en la cuenta bancaria si es 
+     * mayor que 0 actualiza el saldo, si es menor notifica el error.
+     * @param cantidad 
+     */
+    public void ingreso(double cantidad) {
+        if (cantidad > 0) {
+            setSaldo(getSaldo() + cantidad);
+            getMovimientos().add("Ingreso de " + cantidad + " creditos");
+        } else {
+            System.out.println("[ERROR] Ingrtesar una cantidad negativa es un retirada.");
+        }
+    }
+    /**
+     * Función que recibe una cantidad a retirar de la cuenta y realiza 3 comprobaciones:
+     * La primera que la cantidad a retirar sea un numero positivo, si no lo es dará un error.
+     * La segunda si la cuenta tras retirar la cantidad recibida resulta con un saldo menor que -50:
+     *         Si queda con un saldo menor que -50 dara error.
+     *         Si queda por encima retirará esa cantidad de la cuenta y actulizara el saldo.
+     * La tercera si la cantidad es mayor a 3000 avisa al usuario de que se notificará a hacienda el movimiento.
+     * @param cantidad 
+     */
+    public void retirada(double cantidad) {
+        if (cantidad > 0) {
+            if (getSaldo() - cantidad >= -50) {
+                setSaldo(getSaldo() - cantidad);
+                getMovimientos().add("Retirada de " + cantidad + " creditos");
+                if (cantidad > 3000) {
+                    System.out.println("AVISO: Notificar a hacienda");
+                }
+            } else {
+                System.out.println("[ERROR] Saldo negativo, no podemos permitir una cuenta con una deuda mayor a 50 creditos");
+            }
+        } else {
+            System.out.println("[ERROR] Retirar una cantidad negativa es un ingrerso.");
+        }
+    }
+
+    /**
+     * Método de la clase CuentaBancaria que muestra la informacion de la cuenta
+     * (IBAN, titular y saldo)
+     */
+    public void datosCuenta() {
+        System.out.println("IBAN: " + getIban());
+        System.out.println("Titular: " + getTitular());
+        System.out.println("Saldo: " + getSaldo() + " creditos");
+    }
+
+    /**
+     * @return the iban
+     */
+    public String getIban() {
+        return iban;
+    }
+
+    /**
+     * @return the titular
+     */
+    public String getTitular() {
+        return titular;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the movimientos
+     */
+    public ArrayList<String> getMovimientos() {
+        return movimientos;
+    }
+}
---
+++ EjerciciosTEMA8/src/dawbank/DawBank.java
@@ -0,0 +1,73 @@
+package dawbank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class DawBank {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        
+        System.out.println("Bienvenido al sistema de DawBank");
+        System.out.println("Por favor, introduzca los datos de su cuenta bancaria:");
+        System.out.print("IBAN: ");
+        String iban = sc.nextLine();
+        System.out.print("Titular: ");
+        String titular = sc.nextLine();
+        CuentaBancaria c1 = new CuentaBancaria(iban, titular);
+        System.out.println("Cuenta creada correctamente.");
+        
+        do {
+            System.out.println("\nMenu principal:");
+            System.out.println("1. Datos de la cuenta");
+            System.out.println("2. Mostrar IBAN");
+            System.out.println("3. Mostrar Titular");
+            System.out.println("4. Mostrar Saldo");
+            System.out.println("5. Realizar Ingreso");
+            System.out.println("6. Realizar Retirada");
+            System.out.println("7. Mostrar Movimientos");
+            System.out.println("8. Salir");
+            System.out.print("Por favor, seleccione una opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine();//Buffer
+            switch (opcion) {
+                case 1 ->
+                    c1.datosCuenta();
+                case 2 ->
+                    System.out.println("IBAN: " + c1.getIban());
+                case 3 ->
+                    System.out.println("Titular: " + c1.getTitular());
+                case 4 ->
+                    System.out.println("Saldo actual: " + c1.getSaldo() + " euros");
+                case 5 -> {
+                    System.out.print("Cantidad a ingresar: ");
+                    double cantidadIngreso = sc.nextDouble();
+                    sc.nextLine();
+                    c1.ingreso(cantidadIngreso);
+                    System.out.println("Ingreso realizado");
+                }
+                case 6 -> {
+                    System.out.print("Cantidad a retirar: ");
+                    double cantidadRetirada = sc.nextDouble();
+                    sc.nextLine();
+                    c1.retirada(cantidadRetirada);
+                    System.out.println("Saldo restante: " + c1.getSaldo());
+                }
+                case 7 -> {
+                    System.out.println("Movimientos:");
+                    for (String movimiento : c1.getMovimientos()) {
+                        System.out.println(movimiento);
+                    }
+                }
+                case 8 ->
+                    System.out.println("Hasta la proxima");
+                default ->
+                    System.out.println("[ERROR] por favor introduzca una de las opciones del menu.");
+            }
+        } while (opcion != 8);
+    }
+}
