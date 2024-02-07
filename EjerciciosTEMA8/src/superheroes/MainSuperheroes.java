7dda15bbcb02abaf682e9bb2b8577f7b59168d2b
RubenGarrod <151464813+RubenGarrod@users.noreply.github.com>
2024-02-07 16:25:44 +0100

    superheroes finalizado

--- EjerciciosTEMA8/src/superheroes/MainSuperheroes.java
+++ EjerciciosTEMA8/src/superheroes/MainSuperheroes.java
@@ -12,47 +12,64 @@ public class MainSuperheroes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        Coleccion coleccionVacia = new Coleccion("Ninguna Coleccion seleccionada", new ArrayList<Figura>());
         
-        Coleccion coleccion = new Coleccion("Coleccion vacia", new ArrayList<Figura>());
-        Superheroe superheroe1 = new Superheroe("Capitán Rayo");
+        Superheroe superheroe1 = new Superheroe("Capitan Rayo");
         superheroe1.setDescripcion("Controla la electricidad y vuela a la velocidad del rayo.");
         superheroe1.setCapa(true);
 
         Superheroe superheroe2 = new Superheroe("Sombra Nocturna");
         superheroe2.setDescripcion("Maestro del sigilo y las artes marciales. Siempre en la sombra.");
         superheroe2.setCapa(false);
+        
+        Superheroe superheroe3 = new Superheroe("Superman");
+        superheroe3.setDescripcion("El mismisimo superman es super y es man");
+        superheroe3.setCapa(true);
 
-        // Crear dimensiones ficticias para las figuras
-        Dimension dimension1 = new Dimension(0.2, 0.1, 0.3);
-        Dimension dimension2 = new Dimension(0.15, 0.12, 0.25);
 
-        // Crear figuras ficticias asociadas a los superhéroes
+        Dimension dimension1 = new Dimension(120, 15, 20);
+        Dimension dimension2 = new Dimension(70, 40, 40);
+        Dimension dimension3 = new Dimension(205, 120, 70);
+
         Figura figura1 = new Figura("FIG001", 29.99, superheroe1, dimension1);
         Figura figura2 = new Figura("FIG002", 24.99, superheroe2, dimension2);
+        Figura figura3 = new Figura("FIG003", 49.99, superheroe3, dimension2);
+        Figura figura4 = new Figura("FIG004", 99.99, superheroe3, dimension3);
 
-        // Crear una colección llamada "Colección Héroes"
-        Coleccion coleccionHeroes = new Coleccion("Colección Héroes", new ArrayList<>());
+        Coleccion coleccionHeroes = new Coleccion("Coleccion Heroes", new ArrayList<>());
         coleccionHeroes.añadirFigura(figura1);
         coleccionHeroes.añadirFigura(figura2);
 
-        // Crear otra colección llamada "Colección Míticos"
-        Coleccion coleccionMíticos = new Coleccion("Colección Míticos", new ArrayList<>());
-
+        Coleccion coleccionMiticos = new Coleccion("Coleccion Miticos", new ArrayList<>());
+        coleccionMiticos.añadirFigura(figura3);
+        coleccionMiticos.añadirFigura(figura4);
 
 
         while (true) {
             System.out.println("\n MENU DE OPCIONES");
-            System.out.println("\t1. Añadir figura");
-            System.out.println("\t2. Subir precio de figura");
-            System.out.println("\t3. Mostrar figuras con capa");
-            System.out.println("\t4. Figura más valiosa");
-            System.out.println("\t5. Valor total de la colección");
-            System.out.println("\t6. Volumen total de la colección");
+            System.out.println("Coleccion actual: " + coleccionVacia.getNombreColeccion());
+            System.out.println("\t1. Seleccionar Coleccion para operar");
+            System.out.println("\t2. Registrar nueva figura");
+            System.out.println("\t3. Subir precio de un figura");
+            System.out.println("\t4. Mostrar figuras con capa");
+            System.out.println("\t5. Figura mas valiosa");
+            System.out.println("\t6. Valor total de la coleccion");
+            System.out.println("\t7. Volumen total de la coleccion");
             System.out.println("\t0. Salir");
             int opcion = sc.nextInt();
 
             switch (opcion) {
                 case 1 -> {
+                    System.out.println("\t1. Coleccion Heroes");
+                    System.out.println("\t2. Coleccion Miticos");
+                    int eleccion = sc.nextInt();
+                    if (eleccion == 1){
+                        coleccionVacia = coleccionHeroes;
+                    } else if (eleccion == 2){
+                        coleccionVacia = coleccionMiticos;
+                    }
+                }
+                case 2 -> {
                     System.out.println("Ingrese los detalles de la figura:");
                     System.out.println("Codigo: ");
                     String codigo = sc.next();
@@ -66,7 +83,7 @@ public class MainSuperheroes {
                     Superheroe superheroe = new Superheroe(nombreSuperheroe);
 
                     System.out.print("Descripcion de la figura: ");
-                    String descripcion = sc.next();
+                    String descripcion = sc.nextLine();
                     superheroe.setDescripcion(descripcion);
                     
                     System.out.print("¿Lleva capa? (true/false): ");
@@ -85,26 +102,26 @@ public class MainSuperheroes {
                     Dimension dimension = new Dimension(alto, ancho, profundidad);
                     Figura figura = new Figura(codigo, precio, superheroe, dimension);
                     
-                    coleccion.añadirFigura(figura);
+                    coleccionVacia.añadirFigura(figura);
 
                     System.out.println("Figura agregada correctamente.");
                 }
-                case 2 -> {
+                case 3 -> {
                     System.out.print("Ingresa el codigo de la figura: ");
                     String codigo = sc.next();
                     try {
                         System.out.print("Ingrese la cantidad (en euros) en la que aumentar el precio: ");
                         double cantidad = sc.nextDouble();
-                        coleccion.subirPrecio(cantidad, codigo);
+                        coleccionVacia.subirPrecio(cantidad, codigo);
                     } catch (PrecioException e) {
                         System.out.println(e.getMessage());
                     }
                 }
 
-                case 3 -> System.out.println(coleccion.conCapa());
-                case 4 -> coleccion.masValiosa();
-                case 5 -> System.out.println("Valor total de la coleccion: " + coleccion.getValorColeccion());
-                case 6 -> System.out.println("Volumen total de la coleccion: " + coleccion.getVolumenColeccion());
+                case 4 -> System.out.println(coleccionVacia.conCapa());
+                case 5 -> coleccionVacia.masValiosa();
+                case 6 -> System.out.println("Valor total de la coleccion: " + coleccionVacia.getValorColeccion());
+                case 7 -> System.out.println("Volumen total de la coleccion: " + coleccionVacia.getVolumenColeccion());
                 case 0 -> {
                     System.out.println("Programa finalizado.");
                     return;
